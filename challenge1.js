6fdca04f301c46b260dd4d1c583b4aaa8061675c
Osman Hadid <manohadid8@gmail.com>
2021-12-03 23:11:43 +0430

    first challenge

---
+++ challenge1.js
@@ -0,0 +1,10 @@
+let alphabets = "abcdefghijklmnopqrstuvwxyz";
+let alphabetsHeight = [];
+
+for (let i = 0; i < word.length; i++) {
+    alphabetsHeight.push(h[alphabets.indexOf(word[i])]);
+}
+
+var t = alphabetsHeight.sort() [alphabetsHeight.length-1];
+
+return t * word.length;
